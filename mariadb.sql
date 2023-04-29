c56f89adfceeb2ee41953f86599fe95887ad48fe
EMEDIGITAL <daniel@e-me.co>
2023-04-29 09:05:21 -0500

    Create mariadb.sql

---
+++ mariadb.sql
@@ -0,0 +1,36 @@
+SET @database_name = 'experience_system';
+
+SET @table_query = CONCAT(
+  'SELECT ',
+  '  CONCAT(\'{"name":"\', name, \'","request":{"url":"\', request_url, \'","method":"GET"},"response":[]}\') AS item ',
+  'FROM (',
+  '  SELECT TABLE_NAME AS name, CONCAT(\'{{base_url}}/\', REPLACE(TABLE_NAME, \'_\', \'-\')) AS request_url ',
+  '  FROM INFORMATION_SCHEMA.TABLES ',
+  '  WHERE TABLE_TYPE = \'BASE TABLE\' AND TABLE_SCHEMA = ?',
+  ') AS t'
+);
+
+SET @stmt = @table_query;
+PREPARE stmt FROM @stmt;
+EXECUTE stmt USING @database_name;
+DEALLOCATE PREPARE stmt;
+
+SET @json = CONCAT(
+  '{',
+  '  "info": {',
+  '    "_postman_id": "XXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",',
+  '    "name": "Table Endpoints",',
+  '    "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"',
+  '  },',
+  '  "item": [',
+  '    ', (SELECT GROUP_CONCAT(item SEPARATOR ',\n')
+            FROM (SELECT TABLE_NAME AS name,
+                         CONCAT('{{base_url}}/', REPLACE(TABLE_NAME, '_', '-')) AS request_url,
+                         CONCAT('{"name":"', TABLE_NAME, '","request":{"url":"{{base_url}}/', REPLACE(TABLE_NAME, '_', '-'), '","method":"GET"},"response":[]}') AS item
+                  FROM INFORMATION_SCHEMA.TABLES
+                  WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = @database_name) AS t), '',
+  '  ]',
+  '}'
+);
+
+SELECT @json AS postman_collection;
