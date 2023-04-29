2a9fd370d1d3243ed6f08b86d2386a39d78111ea
EMEDIGITAL <daniel@e-me.co>
2023-04-29 09:30:31 -0500

    Update mariadb.sql
    folders and all method added

--- mariadb.sql
+++ mariadb.sql
@@ -1,20 +1,3 @@
-SET @database_name = 'experience_system';
-
-SET @table_query = CONCAT(
-  'SELECT ',
-  '  CONCAT(\'{"name":"\', name, \'","request":{"url":"\', request_url, \'","method":"GET"},"response":[]}\') AS item ',
-  'FROM (',
-  '  SELECT TABLE_NAME AS name, CONCAT(\'{{base_url}}/\', REPLACE(TABLE_NAME, \'_\', \'-\')) AS request_url ',
-  '  FROM INFORMATION_SCHEMA.TABLES ',
-  '  WHERE TABLE_TYPE = \'BASE TABLE\' AND TABLE_SCHEMA = ?',
-  ') AS t'
-);
-
-SET @stmt = @table_query;
-PREPARE stmt FROM @stmt;
-EXECUTE stmt USING @database_name;
-DEALLOCATE PREPARE stmt;
-
 SET @json = CONCAT(
   '{',
   '  "info": {',
@@ -26,11 +9,14 @@ SET @json = CONCAT(
   '    ', (SELECT GROUP_CONCAT(item SEPARATOR ',\n')
             FROM (SELECT TABLE_NAME AS name,
                          CONCAT('{{base_url}}/', REPLACE(TABLE_NAME, '_', '-')) AS request_url,
-                         CONCAT('{"name":"', TABLE_NAME, '","request":{"url":"{{base_url}}/', REPLACE(TABLE_NAME, '_', '-'), '","method":"GET"},"response":[]}') AS item
+                         CONCAT('{"name":"', TABLE_NAME, '", "item": [',
+                                '{"name":"GET","request":{"url":"{{base_url}}/', REPLACE(TABLE_NAME, '_', '-'), '","method":"GET"},"response":[]},',
+                                '{"name":"POST","request":{"url":"{{base_url}}/', REPLACE(TABLE_NAME, '_', '-'), '","method":"POST"},"response":[]},',
+                                '{"name":"PUT","request":{"url":"{{base_url}}/', REPLACE(TABLE_NAME, '_', '-'), '","method":"PUT"},"response":[]},',
+                                '{"name":"PATCH","request":{"url":"{{base_url}}/', REPLACE(TABLE_NAME, '_', '-'), '","method":"PATCH"},"response":[]}',
+                                ']}') AS item
                   FROM INFORMATION_SCHEMA.TABLES
                   WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = @database_name) AS t), '',
   '  ]',
   '}'
 );
-
-SELECT @json AS postman_collection;
